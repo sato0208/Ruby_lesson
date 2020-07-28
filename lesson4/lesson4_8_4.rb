a648562392f6313875a106d2f782691c1584fe7b
sato chiharu <kujira028@gmail.com>
2020-07-28 12:20:05 +0000

    eachメソッドで繰り返し処理をすると配列がブロック引数になる

---
+++ lesson4/lesson4_8_4.rb
@@ -0,0 +1,17 @@
+# 配列がブロック引数に渡される場合
+dimensions = [
+  # [縦、横]
+  [10, 20],
+  [30, 40],
+  [50, 60],
+]
+
+# eachメソッドで繰り返した場合
+areas = []
+dimensions.each do |dimension|
+  length = dimensions[0]
+  width = dimensions[1]
+  areas << length * width
+end
+
+areas => [200, 1200, 3000]
