1ab5e8872107e99f2a6fa236eef6811e3ac63ebe
sato chiharu <kujira028@gmail.com>
2020-08-02 15:51:42 +0000

    while文でのbreak処理

--- lesson4/lesson4_10_1.rb
+++ lesson4/lesson4_10_1.rb
@@ -6,3 +6,13 @@ numbers.each do |n|
   puts n
   break if n == 5
 end
+
+# while文でのbreak処理
+numbers = [1, 2, 3, 4, 5].shuffle
+i = 0
+while i < numbers.size
+  n = numbers[i]
+  puts n
+  break if n == 5
+  i += 1
+end
