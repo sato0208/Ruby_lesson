63870376ae7572e68fa02a45c108db9b18e9e430
sato chiharu <kujira028@gmail.com>
2020-08-02 15:30:26 +0000

    until文を使用した繰り返し処理

--- lesson4/lesson4_9_4.rb
+++ lesson4/lesson4_9_4.rb
@@ -1,6 +1,6 @@
-while 条件式（死んであれば実行）
-  繰り返したい処理
-end
+# while 条件式（真であれば実行）
+#   繰り返したい処理
+# end
 
 0 = []
 while a.size <5
@@ -12,3 +12,12 @@ a
 
 # 一行で書く
 while a.size < 5 do a << 1 end
+
+  # until 条件式（偽あれば実行）
+  #   繰り返したい処理
+  # end
+
+  0 = []
+  until a.size <= 3
+    a.delete_at(-1)
+  end
