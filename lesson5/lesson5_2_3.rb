f9fd17258ccfb083f60838a88231791287d751d4
sato chiharu <kujira028@gmail.com>
2020-08-03 06:48:03 +0000

    ハッシュの要素の削除

--- lesson5/lesson5_2_3.rb
+++ lesson5/lesson5_2_3.rb
@@ -10,3 +10,12 @@ a == b
 
 # 3
 {'x' => 1, 'y' => 2, 'z' => 3}.size
+
+# xと1が削除される
+a.delete('x')
+
+# tがないのでnilがかえる
+a.delete('t')
+
+# ブロックを渡すとキーが見つからない時の戻り値を作成できる
+a.delete('italy'){|key| "Not found: #{key}"}
