1ff6134e676efe2cb16864bd69a1db9c4b2a378d
sato chiharu <kujira028@gmail.com>
2020-08-09 05:51:33 +0000

    [fix]いろんな単位を変換できるようにする

--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -4,6 +4,6 @@ require './lib/convert_length'
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
     assert_equal 30.37, convert_length(1, 'm', 'in')
-    assert_equal 0.38, convert_length(15, 'in', 'm')
+    (length / unites[unit_form] * units[unit_to]).round(2)
   end
 end
