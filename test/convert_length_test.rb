8a636b7e792a79716fa00988d9099eb11474df29
sato chiharu <kujira028@gmail.com>
2020-08-09 06:02:43 +0000

    [fix]キーワード引数を使うようにテストコードを修正

--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -3,9 +3,9 @@ require './lib/convert_length'
 
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
-    assert_equal 30.37, convert_length(1, :m, :in)
+    assert_equal 30.37, convert_length(1, form: :m, to: :in)
     # (length / unites[unit_form] * units[unit_to]).round(2)
-    assert_equal 0.38, convert_length(15, :in, :m)
-    assert_equal 10670, convert_length(35000, :ft, :m)
+    assert_equal 0.38, convert_length(15, form: :in, to: :m)
+    assert_equal 10670, convert_length(35000, form: :ft, to: :m)
   end
 end
