193bd6d379ad40af3d729d48ee4f6f99a2660602
sato chiharu <kujira028@gmail.com>
2020-08-10 05:03:48 +0000

    ハッシュで使用頻度の高いメソッド

---
+++ lesson5/lesson5_6_1.rb
@@ -0,0 +1,12 @@
+# ハッシュで使用頻度の高いメソッド
+# keys
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.keys
+
+# values
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.values
+
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.has_key?(:japan)
+currencies.has_key?(:italy)
