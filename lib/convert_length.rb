6f9757cc46f98b854ee178eaf1fbc64a9d7cdf6c
sato chiharu <kujira028@gmail.com>
2020-08-10 04:57:20 +0000

    ハッシュをメソッドの外に移動させる

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,4 @@
 def convert_length(length, unit_form, unit_to)
   units = { m: 1.0, ft: 3.28, in: 39.37 }
-  (length / units[units_form] * units[unit_to]).round(2)
+  (length / UNITS[form] * UNITS[to]).round(2)
 end
