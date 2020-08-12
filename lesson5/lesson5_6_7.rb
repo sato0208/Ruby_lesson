41f9197f7b9873de88347ff57c7f3212d8c3c0c0
sato chiharu <kujira028@gmail.com>
2020-08-12 07:14:54 +0000

    ハッシュから配列へ、配列からハッシュへ

--- lesson5/lesson5_6_7.rb
+++ lesson5/lesson5_6_7.rb
@@ -8,3 +8,15 @@ array.to_h
 
 array = [1, 2, 3, 4]
 array.to_h
+
+# 重複した場合は最後に登場した配列の要素がハッシュの値に採用される
+array = [[:japan, "yen"], [:japan, "円"]]
+array.to_h
+
+# キーと値のペアの配列をhash[]に渡す
+array = [[:japan, "yen"], [:japan, "円"]]
+Hash[array]
+
+# キーと値が交互に並ぶフラットな配列をsplat展開
+array = [[:japan, "yen"], [:us, "dollar"], [:india, "rupee"]]
+Hash[*array]
