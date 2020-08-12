da5755dfa8571332dfa677a53dff128140727263
sato chiharu <kujira028@gmail.com>
2020-08-12 07:24:03 +0000

    ハッシュの初期値に対して破壊的な変更をした場合の挙動

--- lesson5/lesson5_6_8.rb
+++ lesson5/lesson5_6_8.rb
@@ -6,3 +6,16 @@ h[:foo]
 # キーがなければ'hello'を返す
 h = Hash.new('hello')
 h[:foo]
+
+h = Hash.new('hello')
+a = h[:foo]
+b = h[:bar]
+
+# 変数aと変数bは同一オブジェクト
+a.equal?(b)
+
+# 変数aに破壊的な変更をすると、変数bの値も一緒に変わってしまう
+a.upcase!
+
+# ハッシュはからのまま
+h
