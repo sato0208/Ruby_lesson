4d4033d6f165e3fe54317c7f142dfe228a21d362
sato chiharu <kujira028@gmail.com>
2020-08-12 07:09:24 +0000

    配列をハッシュに変換する

--- lesson5/lesson5_6_7.rb
+++ lesson5/lesson5_6_7.rb
@@ -1,3 +1,10 @@
 # ハッシュから配列へ、配列からハッシュへ
 currencies = japan: 'yen', us: 'dollar', india: 'rupee'
 currencies.to_a
+
+# 配列をハッシュに変換する
+array = [[:japan, "yen"], [:us, "dollar"], [:india, "rupee"]]
+array.to_h
+
+array = [1, 2, 3, 4]
+array.to_h
